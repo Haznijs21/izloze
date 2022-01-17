c1f27df33e544e57461362118c733488929a8c2a
Haznijs21 <bombadourada1@gmail.com>
2022-01-17 15:04:05 +0200

    sakums

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi = ['Andrew Newik', 'John Tree', 'Egil Latvian', 'Sasha Krasnow', 'John Quick'];
+const balvas = ['Kartons', 'Kaste', 'Miljons', 'viens euro', 'rolltons'];
+const naudaKopa = 100000;
+let uzvaretajuSkaits = 5;
+let rand = Math.floor(Math.random() * 5);
+let rand1 = Math.floor(Math.random() * 5);
+console.log(vardi[rand] + " uzvareja " + balvas[rand1]);
