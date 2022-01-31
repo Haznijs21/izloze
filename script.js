08931eaf2c6027eb4e8a06ebea71909ca2dd8e36
Haznijs21 <bombadourada1@gmail.com>
2022-01-31 15:05:37 +0200

    4

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const vardi = ['Andrew Newik', 'John Tree', 'Egil Latvian', 'Sasha Krasnow', 'John Quick'];
-const balvas = ['Kartons', 'Kaste', 'Miljons', 'viens euro', 'rolltons'];
+const balvas = ['Kartons', 'Kaste', 'viens euro', 'rolltons', 'Miljons'];
 const naudaKopa = 100000;
 let uzvaretajuSkaits = vardi.length;
 let rindas = document.querySelector('.rindas');
 rindas.innerHTML = ' ';
+let miljons = 0;
 for(let i = 0; i < uzvaretajuSkaits; i++) {
     let rand = Math.floor(Math.random() * uzvaretajuSkaits);
     let uzvaretajs = vardi[rand];
 
     let rand1 = Math.floor(Math.random() * uzvaretajuSkaits);
     let balva = balvas[rand1];
-    while(balva === 'Miljons') {
-        let rand1 = Math.floor(Math.random() * uzvaretajuSkaits);
-        let balva = balvas[rand1];
+    if (balva == 'Miljons' && miljons < 1) {
+        miljons++;
+    } else if (miljons >= 1) {
+        rand1 = Math.floor(Math.random() * 4);
+        balva = balvas[rand1];
     }
     rindas.innerHTML+= `
     <tr>
