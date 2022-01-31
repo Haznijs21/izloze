5bd7b87b726a1d62871e0e796afe76e670353a8b
Haznijs21 <bombadourada1@gmail.com>
2022-01-31 14:50:09 +0200

    3

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const vardi = ['Andrew Newik', 'John Tree', 'Egil Latvian', 'Sasha Krasnow', 'John Quick'];
 const balvas = ['Kartons', 'Kaste', 'Miljons', 'viens euro', 'rolltons'];
 const naudaKopa = 100000;
-let uzvaretajuSkaits = 5;
-let rand = Math.floor(Math.random() * 5);
-let rand1 = Math.floor(Math.random() * 5);
-console.log(vardi[rand] + " uzvareja " + balvas[rand1]);
+let uzvaretajuSkaits = vardi.length;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+for(let i = 0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.floor(Math.random() * uzvaretajuSkaits);
+    let uzvaretajs = vardi[rand];
+
+    let rand1 = Math.floor(Math.random() * uzvaretajuSkaits);
+    let balva = balvas[rand1];
+    while(balva === 'Miljons') {
+        let rand1 = Math.floor(Math.random() * uzvaretajuSkaits);
+        let balva = balvas[rand1];
+    }
+    rindas.innerHTML+= `
+    <tr>
+        <td>${i+1}</td>
+        <td>${uzvaretajs}</td>
+        <td>${balva}</td>
+    </tr>`
+}
